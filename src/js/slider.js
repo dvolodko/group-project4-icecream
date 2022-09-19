51ebe5585b07906bfd397750b67f672c4a176f20
Alexander <107132921+niksemovs@users.noreply.github.com>
2022-09-19 21:12:20 +0300

    gallary SpaceBetween

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,6 +5,7 @@ const swiper = new Swiper('.swiper', {
     delay: 3000,
     disableOnInteraction: false,
   },
+  spaceBetween: 30,
   loopedSlides: 1,
   pagination: {
     el: '.swiper-pagination',
