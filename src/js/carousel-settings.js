be2b1b359210b45559c1406d1e7060037302c8a5
MarharytaHolub <109298513+MarharytaHolub@users.noreply.github.com>
2022-09-21 13:13:18 +0300

    ці зміни прцюють не коректно

--- src/js/carousel-settings.js
+++ src/js/carousel-settings.js
@@ -1,4 +1,4 @@
-$('.customer__item ,.modal__slider').slick({
+$('.customer__item').slick({
   centerMode: false,
   centerPadding: '60px',
   slidesToShow: 1,
@@ -139,3 +139,60 @@ $('.customer__item3').slick({
     },
   ],
 });
+
+$('.modal__slider').slick({
+  centerMode: false,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  dots: true,
+  appendDots:('.dotes-class'),
+  autoplay: false,
+   });
+
+   $('.modal__slider').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    dots: true,
+    appendDots:('.dotes-class'),
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
