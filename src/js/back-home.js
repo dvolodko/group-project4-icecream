3c3faa59658887e9ef46bfdbb22b3ef2fd51ded6
Denys Volodko <babyleejane@gmail.com>
2022-09-21 19:39:29 +0300

    Update back-home.js

--- src/js/back-home.js
+++ src/js/back-home.js
@@ -1,42 +1,48 @@
-function backToTop()
-{
-    let button = $('.back-to-home');
-    $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 50)
-        {
-            button.fadeIn();
-        }else{
-            button.fadeOut();
-        }
-    });
-    button.on('click', (e) => {
-        e.preventDefault();
-        $('html').animate({scrollTop: 0}, 1000);
-    })
+function backToTop() {
+  let button = $('.back-to-home');
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 50) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+  button.on('click', e => {
+    e.preventDefault();
+    $('html').animate({ scrollTop: 0 }, 1000);
+  });
 }
 backToTop();
 
-
 var $page = $('html, body');
-$('a[href*="#about"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
-    return false;
+$('a[href*="#about"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    300
+  );
+  return false;
 });
 
 var $page = $('html, body');
-$('a[href*="#products"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
-    return false;
+$('a[href*="#products"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    500
+  );
+  return false;
 });
 
 var $page = $('html, body');
-$('a[href*="#contacts"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 3000);
-    return false;
+$('a[href*="#contacts"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    1000
+  );
+  return false;
 });
