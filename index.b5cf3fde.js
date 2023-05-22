b1a10b5c08d6547217e813fa92dbca4f29fc8f38
dvolodko <41117606+dvolodko@users.noreply.github.com>
2023-05-22 13:13:37 +0000

    Deploying to gh-pages from @ dvolodko/group-project4-icecream@8806e5faa588c76cc1f26f93c3557a00799647d1 🚀

---
+++ index.b5cf3fde.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,grabCursor:!0,autoplay:{delay:3e3,disableOnInteraction:!1},spaceBetween:30,loopedSlides:1,pagination:{el:".swiper-pagination",clickable:!0},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.b5cf3fde.js.map
